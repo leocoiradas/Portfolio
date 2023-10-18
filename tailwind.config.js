364acf6fc4bec8cc824814b49c1138207b3663de
leocoiradas <coiradasalan@gmail.com>
2023-10-18 10:42:17 -0300

    Añadido de imagen de fondo a las clases de tailwind
    Se añadió la imagen que se usará como fondo a la lista de clases de tailwind, modificando la propiedad extend añadiendo la imagen a la clase backgroundImage con el nombre de "space-background".

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ export default {
   content: ["./index.html",
   "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'space-background': "url('./assets/background-image/space.jpg')"
+      }
+    },
   },
   plugins: [],
 }
