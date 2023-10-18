e3f62d42158ca36efb6fec0326a773a97048f73f
leocoiradas <coiradasalan@gmail.com>
2023-10-18 20:13:24 -0300

    Implementación de Header y fondo de página
    Se implementó el Header, ahora aparece el encabezado del sitio web junto con el navbar con las secciones Home, About me, Projects y Contact. Debajo de todo esto está el componente Home que contiene la presentación con el mensaje.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home"
+import Header from "./pages/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className="bg-space-background">
+      <Header />
       <Home />
-    </>
+    </div>
   );
 }
 
