07dedfc2dff3939b7d229a027c86f866b4a0c63a
leocoiradas <coiradasalan@gmail.com>
2023-10-18 20:11:03 -0300

    Remoción de el fondo espacial
    Se removió el fondo espacial del home. La idea es que el fondo ocupe toda la pantalla incluyendo la zona del navbar por lo que para hacer pruebas se pondrá dicho fondo en el componente App para testear como queda.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Introduction from "../components/Introduction";
 
+
 function Home(){
     return(
-        <main className="flex flex-col justify-center items-center w-screen h-screen bg-space-background gap-8">
+        <main className="flex flex-col justify-center items-center w-full h-screen gap-8">
+        
             <h3 className="text-yellow-500 text-center text-3xl">Presentación</h3>
             <Introduction />
         </main>
