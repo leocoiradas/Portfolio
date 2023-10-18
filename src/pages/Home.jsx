a115c580f63e68790e3685ab21f497b3709f70c0
leocoiradas <coiradasalan@gmail.com>
2023-10-18 10:46:31 -0300

    Implementación del fondo al sitio web
    Se implementó el fondo con motivo de espacio exterior al sitio web. Adicionalmente se añadió un mensaje con el nombre de la sección en la página.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,8 @@ import Introduction from "../components/Introduction";
 
 function Home(){
     return(
-        <main className="flex justify-center items-center w-screen h-screen">
+        <main className="flex flex-col justify-center items-center w-screen h-screen bg-space-background gap-8">
+            <h3 className="text-yellow-500 text-center text-3xl">Presentación</h3>
             <Introduction />
         </main>
     )
