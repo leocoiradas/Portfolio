47ae404ecc27f9e3cc70677be227c9107402a07f
leocoiradas <coiradasalan@gmail.com>
2023-10-19 16:27:23 -0300

    Implementación de Link en lugar de un anchor
    Se implementó el componente Link en lugar de el anchor que estaba en el navbar, esto nos permitirá navegar en las secciones de la app renderizando el componente respectivo configurado en el router. Como no están creados otros componentes aparte del home, las demás rutas a las que redireccionarán los botones (gracias al metodo "to") a una ruta donde se renderizará un mensaje que indica que la página está en construcción.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,21 +9,21 @@ function Navbar() {
     },
     {
         name: "About me",
-        path: "/about"
+        path: "*"
     },
     {
       name: "Projects",
-      path: "proyects"
+      path: "*"
     },
     {
        name: "Contact",
-       path: "/contact" 
+       path: "*" 
     },
     
   ];
   return(
     <ul className="flex justify-center items-center gap-4">
-      {links.map((link) => (<a className="px-4 py-2 text-white text-center hover:rounded-md hover:border-2 hover:border-yellow-400 hover:text-yellow-400" href={link.path}>{link.name}</a>))}
+      {links.map((link) => (<Link to={link.path} className="px-4 py-2 text-white text-center hover:rounded-md hover:border-2 hover:border-yellow-400 hover:text-yellow-400">{link.name}</Link>))}
     </ul>
   );
 }
