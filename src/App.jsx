5d7ae95d3051f9c8b3a6b9e818866360fabcf19e
leocoiradas <coiradasalan@gmail.com>
2023-10-19 16:10:54 -0300

    Eliminación de useState
    Se eliminó el useState ya que no se está utilizando en la aplicación.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+
 import "./App.css";
 import Home from "./pages/Home"
 import Header from "./pages/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <div className="bg-space-background">
