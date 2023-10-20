ae53f8cb38425929a33666ae38dfd800e0671693
leocoiradas <coiradasalan@gmail.com>
2023-10-20 17:43:02 -0300

    Creación de Details
    Se creó el componente Details, aquí se creará un cuadro con mis datos personales como nombre, nacionalidad, fecha de nacimiento, foto de perfil, título, etc., el cual será exportado y se usará en una sección aparte que se renderizará cuando el usuario presione en el botón "About me" del navbar.

---
+++ src/components/Details.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import profileImg from "../assets/profile/profile.png"
+
+function Details() {
+  const details = {
+    name: "Alan Leonel Coiradas",
+    title: "Full Stack Developer",
+    birthDate: "14/02/2000",
+    country: "Argentina",
+    city: "Campana",
+    nationality: "argentino",
+  };
+
+  return (
+    <article className="flex justify-center items-center min-w-[60vw] gap-3 p-4 text-2xl text-yellow-500 border-2 rounded-md border-cyan-400 ">
+        <article className="flex flex-col justify-center items-center flex-1">
+            <article className="w-48 h-48">
+                <img src={profileImg} alt={details.name} classname="object-cover w-full h-full" />
+            </article>
+            <h4>{details.name}</h4>
+            <h4>{details.title}</h4>
+      </article>
+      <article className="w-2/3">
+        <p><b>Fecha de nacimiento: </b>{details.birthDate}</p>
+        <p><b>País: </b>{details.country}</p>
+        <p><b>Ciudad: </b>{details.city}</p>
+        <p><b>Nacionalidad: </b>{details.nationality}</p>
+      </article>
+      
+    </article>
+  );
+}
+
+export default Details;
