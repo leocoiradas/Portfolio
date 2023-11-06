de12d0903d55393634db62712cb7c874d594d881
leocoiradas <coiradasalan@gmail.com>
2023-11-06 18:04:52 -0300

    Creación de componente Proyects
    Este componente servirá para mostrar los proyectos realizados por el usuario. El componente tiene acceso al estado de proyects de Redux y ejecuta la petición get para obtener los datos de la colección de proyectos mediante el dispatch que dispara la acción get_proyects. También se implementó el componente Page asignando como props la constante de proyectsText que contiene la información descriptiva de la sección.

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { get_proyects } from "../store/actions/proyectsActions";
+import ProjectCard from "../components/ProjectCard";
+import Page from "../components/Page";
+import { BsCodeSlash } from "react-icons/bs"
+
+function Proyects(){
+    const dispatch = useDispatch()
+    const proyects = useSelector((store) => store.proyectsReducer.proyects)
+    useEffect(() => {
+        dispatch(get_proyects())
+    }, [])
+    const projectsText = {
+        title: "Projects",
+        comment: "These are some of the proyects thar I've been working on. Every single one include some of all the Teccnologies that I learned so far. Feel free to check them out!!",
+        icon: <BsCodeSlash />
+    }
+    return(
+        <Page page = {projectsText}>
+            {proyects.map((element) => (<ProjectCard project={element}/>))}
+        </Page>
+    )
+}
+
+export default Proyects
