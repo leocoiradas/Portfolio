454d0f1a3202f24da2871deae5837b1adef0fc8f
leocoiradas <coiradasalan@gmail.com>
2023-11-07 20:49:47 -0300

    Ajustes en estilos de tailwind (height)
    Se ajustó el alto del componente para que ahora no sea fijo, tenga un mínimo de alto de 90vh y así que tenga la posibilidad de expandirse de ser necesario.

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Page({children}, info){
     return(
-        <section className="flex flex-col justify-center items-center h-[90vh] gap-8 p-3">
+        <section className="flex flex-col justify-center items-center min-h-[90vh] gap-8 p-3">
             <article className="flex flex-col justify-center items-center p-5">
                 <h1>{info.title}</h1>
                 <p>{info.comment}</p>
