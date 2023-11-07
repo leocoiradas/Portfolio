ddd52d286ae5827d7d61f901c399962b59a16b74
leocoiradas <coiradasalan@gmail.com>
2023-11-07 20:39:47 -0300

    Establecida ruta de Projects
    Se estableció el valor de la propiedad path del botón Projects del navbar a "/projects" para redirigir a esa dirección al momento de ser clickeado. Adicionalmente se añadió una propiedad Key a cada botón del navbar al momento de ser mapeado.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar() {
     },
     {
       name: "Projects",
-      path: "*"
+      path: "/projects"
     },
     {
       name: "Studies",
@@ -27,7 +27,7 @@ function Navbar() {
   ];
   return(
     <ul className="flex justify-center items-center gap-2">
-      {links.map((link) => (<Link to={link.path} className="px-4 py-2 text-white text-center hover:rounded-md hover:border-2 hover:border-yellow-400 hover:text-yellow-400">{link.name}</Link>))}
+      {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center hover:rounded-md hover:border-2 hover:border-yellow-400 hover:text-yellow-400">{link.name}</Link>))}
     </ul>
   );
 }
