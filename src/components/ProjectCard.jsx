2dfa462ff11e55b544b8c5320aeffdaeba750cd1
leocoiradas <coiradasalan@gmail.com>
2023-11-07 20:51:47 -0300

    Ajuste de estilos: background color
    Se agregó un color de fondo negro a la tarjeta para mejorar su visibilidad y la vista no se vea estorbada por el fondo del sitio web.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectCard({project}) {
     return (
         <article>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
                 <img className="w-full" src={project.images[0]} alt={project.name} />
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2 text-white">{project.name}</div>
