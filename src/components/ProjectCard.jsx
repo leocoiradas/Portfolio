901fcb17619c5d61b6efa358db66dd83491f0f67
leocoiradas <coiradasalan@gmail.com>
2023-11-07 20:48:28 -0300

    Cambio de class a className + ajustes en los nombres de atributos
    Se cambio el nombre de la propiedad class a className para que sea acorde al uso de React. Adicionalmente se ajustaron los nombres de los atributos en los elementos JSX para que coincidan con los atributos traídos del backend (name en vez de title). Se cambió el color del texto a blanco para hacerlo visible.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function ProjectCard({project}) {
     return (
         <article>
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-                <img class="w-full" src={project} alt={project.title} />
-                <div class="px-6 py-4">
-                    <div class="font-bold text-xl mb-2">{project.title}</div>
-                    <p class="text-gray-700 text-base">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={project.images[0]} alt={project.name} />
+                <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2 text-white">{project.name}</div>
+                    <p className="text-white text-base">
                         {project.shortDesc}
                     </p>
                 </div>
