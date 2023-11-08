f10a46d41d53f330599bde0a05d02b073efc274c
leocoiradas <coiradasalan@gmail.com>
2023-11-08 16:13:52 -0300

    Ajuste en los estilos de la tarjeta

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function ProjectCard({project}) {
     return (
-            <div className="max-w-[30rem] rounded overflow-hidden shadow-lg bg-black ">
-                <div className="w-full">
-                    <img className="w-full h-full object-cover" src={project.images[0]} alt={project.name} />
+            <div className="max-w-[75vw] md:max-w-[38rem] rounded overflow-hidden shadow-lg bg-black ">
+                <div className=" h-[20rem] w-full">
+                    <img className="object-fill w-full h-full" src={project.images[0]} alt={project.name} />
                 </div>
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2 text-white">{project.name}</div>
