faf0e40e222d15075fa1a3eed93dc4421879e1b0
leocoiradas <coiradasalan@gmail.com>
2023-11-08 16:19:50 -0300

    Cambios estéticos
    Cambios estéticos para mejorar la vista en dispositivos móviles

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectCard({project}) {
     return (
             <div className="max-w-[75vw] md:max-w-[38rem] rounded overflow-hidden shadow-lg bg-black ">
-                <div className=" h-[20rem] w-full">
+                <div className="h-[10rem] md:h-[20rem] w-full">
                     <img className="object-fill w-full h-full" src={project.images[0]} alt={project.name} />
                 </div>
                 <div className="px-6 py-4">
