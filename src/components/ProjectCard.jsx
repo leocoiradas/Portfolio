ded9fcc11700e78ca22245967d842f0526acdef4
leocoiradas <coiradasalan@gmail.com>
2023-11-07 21:11:39 -0300

    Cambios de estilos en la card
    Se modificaron los estilos en la card, ajustando su ancho,  el alto y ancho de la imagen para que se vea de mejor manera.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,8 +3,10 @@ import React from "react";
 function ProjectCard({project}) {
     return (
         <article>
-            <div className="max-w-sm rounded overflow-hidden shadow-lg bg-black">
-                <img className="w-full" src={project.images[0]} alt={project.name} />
+            <div className="max-w-[30rem] rounded overflow-hidden shadow-lg bg-black ">
+                <div className="w-full">
+                    <img className="w-full h-full object-cover" src={project.images[0]} alt={project.name} />
+                </div>
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2 text-white">{project.name}</div>
                     <p className="text-white text-base">
