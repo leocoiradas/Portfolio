f20c29eb363a43f0c0fefd95514c732e973314ad
leocoiradas <coiradasalan@gmail.com>
2023-11-08 16:13:30 -0300

    Corrección de errores: prop info
    Se corrigió un error por el cual la información correspondiente a info no funcionaba. Adicionalmente se Agregaron estilos al contenedor y los elementos dentro del contenedor.

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-function Page({children}, info){
+function Page({children, info}){
     return(
-        <section className="flex flex-col justify-center items-center min-h-[90vh] gap-8 p-3">
-            <article className="flex flex-col justify-center items-center p-5">
-                <h1>{info.title}</h1>
+        <section className="flex flex-col justify-center items-center min-h-[90vh] w-full gap-8 p-3">
+            <article className="w-[75vw] flex flex-col justify-center items-center p-5 text-xl text-center text-white gap-5">
+                <h1 className="text-3xl">{info.title}</h1>
                 <p>{info.comment}</p>
-                <p>{info.icon}</p>
+                <p className="text-2xl">{info.icon}</p>
             </article>
             {children}
         </section>
