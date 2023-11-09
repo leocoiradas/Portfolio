ad28347c75903feed8f00736c2eee06288ec8cb8
leocoiradas <coiradasalan@gmail.com>
2023-11-09 16:30:32 -0300

    Creación de ProjectDetails + incorporación de DetailsCard
    Se creó el componente donde se utilizarán los datos almacenados en el store de Redux para renderizar la tarjeta DetailsCard con la información correspondiente a los detalles del proyecto.

---
+++ src/components/ProjectDetails.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { get_proyect_details } from "../store/actions/proyectDetailsAction";
+import DetailsCard from "./DetailsCard";
+
+function ProjectDetails() {
+    const dispatch = useDispatch()
+    const {proyectId} = useParams()
+    const proyectDetails = useSelector((store) => store.proyectDetailsReducer.proyect_details)
+    useEffect(() => {
+        dispatch(get_proyect_details({
+            id: proyectId
+        }))
+    }, [])
+    return (
+        <>
+            <DetailsCard />
+        </>
+    )
+}
+
+export default ProjectDetails
