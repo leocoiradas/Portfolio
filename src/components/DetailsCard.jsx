8e3740795cb4db7700c40216025834599a3b1264
leocoiradas <coiradasalan@gmail.com>
2023-11-09 23:57:55 -0300

    Cambios varios en componente DetailsCard
    1) Se hicieron cambios en las funciones previousPhoto y nextPhoto, se modificó el details.image por details.images para que corresponda a la estructura del atributo de los datos traídos de la api. Adicionalmente se modificó la lógica de los condicionales ternarios de cada función.
    
    2) Se añadió un condicional al return para que cuando details y el array details.images sean truthy (que no sea undefined) se renderice la tarjeta, de lo contrario devolverá un valor null

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 
-function DetailsCard({details}) {
+function DetailsCard({ details }) {
     const [index, setIndex] = useState(0)
     const previousPhoto = () => {
-        setIndex((currentImage) => (currentImage - 1 > 0 ? currentImage - 1 : details.image.length - 1))
+        setIndex((currentImage) => (currentImage - 1 >= 0 ? currentImage - 1 : details.images.length - 1))
     }
     const nextPhoto = () => {
-        setIndex((currentImage) => (currentImage + 1 >= details.image.length ? currentImage + 1 : 0))
+        setIndex((currentImage) => (currentImage + 1 < details.images.length ? currentImage + 1 : 0))
     }
     const imageCarousel = () => {
         nextPhoto()
@@ -19,28 +19,31 @@ function DetailsCard({details}) {
         }
     }, [])
 
-    return(
-        <div
-            class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-            <div class="relative overflow-hidden bg-cover bg-no-repeat">
-                <img
-                    class="rounded-t-lg"
-                    src={details.image[index]}
-                    alt={details.name} />
-            </div>
-            <div class="p-6">
-                <h5
-                    class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
-                    {details.name}
-                </h5>
-                <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
-                    {details.desc}
-                </p>
-                <p class="text-base text-neutral-600 dark:text-neutral-200">
-                    <small class="text-neutral-500 dark:text-neutral-400">{details.period}</small>
-                </p>
-            </div>
-        </div>
+    return (
+        <>
+            {details && details.images ? (<div
+                class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+                <div class="relative overflow-hidden bg-cover bg-no-repeat">
+                    <img
+                        class="rounded-t-lg"
+                        src={details.images[index]}
+                        alt={details.name} />
+                </div>
+                <div class="p-6">
+                    <h5
+                        class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
+                        {details.name}
+                    </h5>
+                    <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                        {details.desc}
+                    </p>
+                    <p class="text-base text-neutral-600 dark:text-neutral-200">
+                        <small class="text-neutral-500 dark:text-neutral-400">{details.period}</small>
+                    </p>
+                </div>
+            </div>) : null}
+
+        </>
     )
 }
 
