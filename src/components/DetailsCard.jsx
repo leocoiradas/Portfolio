0b29ec1c7396f24de2e319ff6085019f0cb8ba38
leocoiradas <coiradasalan@gmail.com>
2023-11-13 16:23:11 -0300

    Ajuste de estilos de la tarjeta DetailsCard
    Se ajustaron un poco los estilos de la tarjeta, el ancho máximo de la misma, cómo se muestra la imagen y la altura de esta.

--- src/components/DetailsCard.jsx
+++ src/components/DetailsCard.jsx
@@ -23,12 +23,12 @@ function DetailsCard({ details }) {
     }, [details]);
 
     return (
-        <>
+        <section className="max-w-[75vw]">
             {details && details.images ? (<div
                 className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                <div className="relative overflow-hidden bg-cover bg-no-repeat">
+                <div className="h-[40rem]">
                     <img
-                        className="rounded-t-lg"
+                        className=" h-full w-full rounded-t-lg object-fill"
                         src={details.images[index]}
                         alt={details.name} />
                         {console.log(details.images[index])}
@@ -47,7 +47,7 @@ function DetailsCard({ details }) {
                 </div>
             </div>) : null}
 
-        </>
+        </section>
     )
 }
 
