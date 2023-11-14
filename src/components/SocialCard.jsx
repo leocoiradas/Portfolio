e5d0bc6e4c9ee8cf629e1f6047f8ad94fd43ec9b
leocoiradas <coiradasalan@gmail.com>
2023-11-14 16:34:25 -0300

    Cambio de nombre de prop socialMedia a socialInfo en SocialCard

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function SocialCard({ socialMedia, color }) {
+function SocialCard({ socialInfo, color }) {
     return (
         <article className={`w-[70vw] flex flex-wrap gap-2 p-2 border-2 rounded-md border-${color}-600 p-3`}>
             <div className="w-1/3">
                 <figure className={`text-${color} text-2xl`}>
-                    {socialMedia.icon}
+                    {socialInfo.icon}
                 </figure>
             </div>
             <div className="flex-1">
-                <h2 className={`text-${color} text-2xl`}>{socialMedia.website}</h2>
-                <p className="text-xl">{socialMedia.message}</p>
-                <Link to={socialMedia.to} className={`bg-${color}-500 hover:bg-${color}-700 rounded-md p-3`}>Go</Link>
+                <h2 className={`text-${color} text-2xl`}>{socialInfo.website}</h2>
+                <p className="text-xl">{socialInfo.message}</p>
+                <Link to={socialInfo.to} className={`bg-${color}-500 hover:bg-${color}-700 rounded-md p-3`}>Go</Link>
             </div>
         </article>
     )
