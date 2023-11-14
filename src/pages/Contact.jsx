ee2de4f0b546fda8ffa7fe5a725882669601f0e4
leocoiradas <coiradasalan@gmail.com>
2023-11-14 18:14:19 -0300

    Creado componente de la sección Contact
    En esta sección se mostrará la información correspondiente a las redes de contacto.

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Page from "../components/Page";
+import ContactInfo from "../components/ContactInfo";
+import { BiMessageDetail } from "react-icons/bi";
+
+function Contact(){
+    const contactText = {
+        title: "Contact",
+        comment: "Do you want to have a direct contact with me? Here you have my social media accounts. Contact me whenever you want and I'll respond as soon as I can.",
+        icon: <BiMessageDetail />
+    }
+    return(
+        <Page info={contactText}>
+            <ContactInfo />
+        </Page>
+    )
+}
+export default Contact
