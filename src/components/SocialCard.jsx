6452882453cd8b0a4cf161dd8f374dab380d3b0f
leocoiradas <coiradasalan@gmail.com>
2023-11-14 18:54:13 -0300

    Cambio de estilos, eliminación de prop color
    Se eliminó la prop color por problemas de compatibilidad y se reemplazó por estilos fijos. Se aplicaron más clases para ajustar los estilos de las tarjetas.

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 function SocialCard({ socialInfo }) {
     return (
-        <article className={`w-[70vw] flex flex-wrap gap-2 p-2 border-2 rounded-md border-${socialInfo.color}-600 p-3`}>
+        <article className={`w-[70vw] flex justify-center items-center flex-wrap gap-2 border-2 rounded-md border-purple-600 p-3`}>
             <div className="w-1/3">
-                <figure className={`text-${socialInfo.color} text-2xl`}>
+                <figure className="flex justify-center items-center text-purple-500 text-5xl">
                     {socialInfo.icon}
                 </figure>
             </div>
-            <div className="flex-1">
-                <h2 className={`text-${socialInfo.color} text-2xl`}>{socialInfo.website}</h2>
-                <p className="text-xl">{socialInfo.message}</p>
-                {socialInfo.mail ? (<a href={socialInfo.to} className={`bg-${socialInfo.color}-500 hover:bg-${socialInfo.color}-700 rounded-md p-3`}>Go</a>)
+            <div className="flex flex-col justify-center items-start flex-1 gap-3">
+                <h2 className={"font-sans text-purple-500 text-2xl"}>{socialInfo.website}</h2>
+                <p className="font-mono text-xl text-white">{socialInfo.message}</p>
+                {socialInfo.mail ? (<a href={`mailto:${socialInfo.to}`} className={"bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2"}>Go</a>)
                 :
-                <a href={`mailto:${socialInfo.to}`} className={`bg-${socialInfo.color}-500 hover:bg-${socialInfo.color}-700 rounded-md p-3`}>Go</a>}
+                <a href={socialInfo.to} className={"bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2"}>Go</a>}
             </div>
         </article>
     )
