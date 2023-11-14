7d422e6f9174a9b966166f7dd1041b1ee09126c5
leocoiradas <coiradasalan@gmail.com>
2023-11-14 16:40:19 -0300

    Cambio en la lógica de prop color en SocialCard
    Se modificó la lógica de la prop color a raíz de los cambios hechos en ContactInfo. Se adaptó el manejo de la prop color para adaptarla al nuevo manejo de la propiedad.

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function SocialCard({ socialInfo, color }) {
+function SocialCard({ socialInfo }) {
     return (
-        <article className={`w-[70vw] flex flex-wrap gap-2 p-2 border-2 rounded-md border-${color}-600 p-3`}>
+        <article className={`w-[70vw] flex flex-wrap gap-2 p-2 border-2 rounded-md border-${socialInfo.color}-600 p-3`}>
             <div className="w-1/3">
-                <figure className={`text-${color} text-2xl`}>
+                <figure className={`text-${socialInfo.color} text-2xl`}>
                     {socialInfo.icon}
                 </figure>
             </div>
             <div className="flex-1">
-                <h2 className={`text-${color} text-2xl`}>{socialInfo.website}</h2>
+                <h2 className={`text-${socialInfo.color} text-2xl`}>{socialInfo.website}</h2>
                 <p className="text-xl">{socialInfo.message}</p>
-                <Link to={socialInfo.to} className={`bg-${color}-500 hover:bg-${color}-700 rounded-md p-3`}>Go</Link>
+                <Link to={socialInfo.to} className={`bg-${socialInfo.color}-500 hover:bg-${socialInfo.color}-700 rounded-md p-3`}>Go</Link>
             </div>
         </article>
     )
