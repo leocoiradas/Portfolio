4953ba5b1df9711211802b15a1c32c997899bf31
leocoiradas <coiradasalan@gmail.com>
2023-11-14 16:38:06 -0300

    Implementación de SocialCard en ContactInfo
    Se añadió el uso del componente SocialCard en ContactInfo, adicionalmente se reemplazó el array de colores y se agregaron los colores dentro del array contact como atributo de cada red social.

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 import { BiLogoGmail } from "react-icons/bi";
+import SocialCard from "./SocialCard";
+
 function ContactInfo(){
 
     const contact = [
@@ -9,26 +11,28 @@ function ContactInfo(){
             website: "Linkedin",
             icon: <BsLinkedin />,
             message: "",
+            color: "blue",
             to: ""
         },
         {
             website: "GitHub",
             icon: <BsGithub />,
             message: "",
+            color: "orange",
             to: ""
         },
         {
             website: "E-Mail",
             icon: <BiLogoGmail />,
             message: "",
+            color: "red",
             to: ""
         }
     ]
-    const colors = ["blue", "orange", "red"]
 
     return(
         <section>
-            {contact.map((element) =>  (<article></article>))}
+            {contact.map((element) =>  (<SocialCard socialInfo={element} />))}
         </section>
     )
 }
