3e1f8203a873f3521497764e41941bb7dba5d524
leocoiradas <coiradasalan@gmail.com>
2023-11-13 22:40:20 -0300

    Adición de estilos + prop color en SocialCard
    Se añadieron estilos a los distintos elementos del componente. Adicionalmente se agregó una prop llamada "color" con la cual se va a personalizar cada tarjeta al momento de mapearla dándole distintos colores a cada tarjeta.

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-function SocialCard({ socialMedia }) {
+function SocialCard({ socialMedia, color }) {
     return (
-        <article className="flex flex-wrap gap-2 p-2">
+        <article className={`w-[70vw] flex flex-wrap gap-2 p-2 border-2 rounded-md border-${color}-600 p-3`}>
             <div className="w-1/3">
-                <figure>
+                <figure className={`text-${color} text-2xl`}>
                     {socialMedia.icon}
                 </figure>
             </div>
             <div className="flex-1">
-                <h2>{socialMedia.website}</h2>
-                <p>{socialMedia.message}</p>
-                <Link to={socialMedia.to}>Go</Link>
+                <h2 className={`text-${color} text-2xl`}>{socialMedia.website}</h2>
+                <p className="text-xl">{socialMedia.message}</p>
+                <Link to={socialMedia.to} className={`bg-${color}-500 hover:bg-${color}-700 rounded-md p-3`}>Go</Link>
             </div>
         </article>
     )
