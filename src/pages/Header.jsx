0ed4f945723cf21ab1e2706c4204e18d0752d019
leocoiradas <coiradasalan@gmail.com>
2023-11-25 20:30:50 -0300

    Ajuste de estilos del header

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,7 +4,7 @@ import Navbar from "../components/Navbar";
 function Header(){
     return(
         <header className="flex justify-around w-full min-h-[10vh] p-3">
-            <div className=" w-2/3">
+            <div className=" w-1/2">
                 <h3 className="text-white">Portfolio</h3>
             </div>
             <Navbar />
