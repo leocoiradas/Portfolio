9d0d6ddd0d7a9c486071f43c02af137bb74a2a9e
leocoiradas <coiradasalan@gmail.com>
2023-11-25 20:58:09 -0300

    Añadido de escala en CertificationCard
    Se añadió una propiedad hover:scale para que cuando el usuario pase el cursor por encima de la tarjeta, esta aumente su tamaño hasta un 110% del tamaño original.

--- src/components/CertificationCard.jsx
+++ src/components/CertificationCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function CertificationCard({ certification }) {
     return (
-        <article className="flex flex-col justify-center items-center w-[25rem] border-2 rounded-md bg-purple-600 p-3 gap-3">
+        <article className="flex flex-col justify-center items-center w-[25rem] border-2 rounded-md bg-purple-600 hover:scale-[110%] p-3 gap-3">
             <div className="max-h-[15rem]">
                 <img src={certification.image} className="w-full h-full rounded-md"/>
             </div>
