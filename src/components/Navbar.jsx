d7dbe669073903dfd49b183d2bf30272b5295cad
leocoiradas <coiradasalan@gmail.com>
2023-11-28 19:10:15 -0300

    Ajuste de Hover a los elementos del navbar
    Se cambiaron los estilos de los botones del navbar cuando les hacen hover. Pasaron de ser de un color amarillo a uno púrpura.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ function Navbar() {
   ];
   return(
     <ul className="flex justify-center items-center gap-2">
-      {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center hover:rounded-md hover:border-2 hover:border-yellow-400 hover:text-yellow-400">{link.name}</Link>))}
+      {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center hover:rounded-md hover:border-2 hover:border-purple-600 hover:text-purple-600">{link.name}</Link>))}
     </ul>
   );
 }
