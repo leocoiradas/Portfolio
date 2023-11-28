250e45dadb327c28c58a2dc6cb709ebea75d7e16
leocoiradas <coiradasalan@gmail.com>
2023-11-28 19:11:52 -0300

    Cambio de color del texto del componente Skills
    Se modificó el color del texto del encabezado del componente así como los de los íconos correspondientes a las habilidades tecnológicas.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,9 +7,9 @@ function Skills(){
     const skillsCollection = [<FaHtml5 />, <FaCss3Alt />, <IoLogoJavascript />, <FaGit />, <FaGithub />, <SiTailwindcss />, <FaReact />, <SiRedux />, <SiMongodb />, <SiExpress />, <FaNodeJs />, <SiPostman />]
     return(
         <article className="flex flex-col justify-center items-center gap-5 py-3 w-4/5 ">
-            <h3 className="text-3xl text-purple-600">Skills</h3>
+            <h3 className="text-3xl text-white">Skills</h3>
             <div className="flex flex-wrap gap-3">
-                {skillsCollection.map((element) => (<p className="p-2 text-white rounded-md text-center text-4xl"><b>{element}</b></p>))}
+                {skillsCollection.map((element) => (<p className="p-2 text-purple-600 rounded-md text-center text-4xl"><b>{element}</b></p>))}
             </div>
             
         </article>
