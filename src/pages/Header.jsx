0145dfa6d5b2b308d461e3d1b038d34ede531dca
leocoiradas <coiradasalan@gmail.com>
2023-11-28 19:14:27 -0300

    Ajuste de altura del header
    Se modificó la altura del Header para que ocupe un 10dvh del dispositivo usado para visualizar la página.

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 
 function Header(){
     return(
-        <header className="flex justify-around w-full min-h-[10vh] p-3">
+        <header className="flex justify-around w-full min-h-[10dvh] p-3">
             <div className=" w-1/2">
                 <h3 className="text-white">Portfolio</h3>
             </div>
