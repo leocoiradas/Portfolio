efb9ad1689c6ff64b05c0a470048ed16a13babca
leocoiradas <coiradasalan@gmail.com>
2023-11-28 16:10:21 -0300

    Creado componente Skills
    Es el reemplazo del componente Technologies. Aquí se reemplazó la sección con los nombres de las habilidades aprendidas por una sección con los íconos de las habilidades conocidas.

---
+++ src/components/Skills.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { FaHtml5, FaCss3Alt, FaGit, FaGithub, FaBootstrap, FaReact, FaNodeJs } from "react-icons/fa";
+import { IoLogoJavascript } from "react-icons/io5";
+import { SiRedux, SiMongodb, SiExpress, SiPostman, SiTailwindcss } from "react-icons/si";
+
+function Skills(){
+    const skillsCollection = [<FaHtml5 />, <FaCss3Alt />, <IoLogoJavascript />, <FaGit />, <FaGithub />, <SiTailwindcss />, <FaReact />, <SiRedux />, <SiMongodb />, <SiExpress />, <FaNodeJs />, <SiPostman />]
+    return(
+        <article className="flex flex-col justify-center items-center gap-5 py-3 w-4/5 ">
+            <h3 className="text-3xl text-purple-600">Skills</h3>
+            <div className="flex flex-wrap gap-3">
+                {skillsCollection.map((element) => (<p className="p-2 text-white rounded-md text-center text-4xl"><b>{element}</b></p>))}
+            </div>
+            
+        </article>
+    )
+}
+
+export default Skills
