1ecd65ae7c5e49ea203a626b8538a4f433ddda4e
leocoiradas <coiradasalan@gmail.com>
2023-11-28 19:13:20 -0300

    Modificación de estilos + texto del botón
    Se modificaron los estilos del botón que redirige a la red social, adicionalmente se modificó el texto del mismo cambiándolo por el ícono + nombre de dicha red social.

--- src/components/SocialCard.jsx
+++ src/components/SocialCard.jsx
@@ -11,9 +11,9 @@ function SocialCard({ socialInfo }) {
             <div className="flex flex-col justify-center items-start flex-1 gap-3">
                 <h2 className={"font-sans text-purple-500 text-2xl"}>{socialInfo.website}</h2>
                 <p className="font-mono text-xl text-white">{socialInfo.message}</p>
-                {socialInfo.mail ? (<a href={`mailto:${socialInfo.to}`} target="_blank" className="bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2">Go</a>)
+                {socialInfo.mail ? (<a href={`mailto:${socialInfo.to}`} target="_blank" className="w-36 inline-flex justify-start items-center bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2 gap-2">{socialInfo.icon} {socialInfo.website}</a>)
                 :
-                <a href={socialInfo.to} target="_blank" className="bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2">Go</a>}
+                <a href={socialInfo.to} target="_blank" className="w-36 inline-flex justify-start items-center bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2 gap-2">{socialInfo.icon} {socialInfo.website}</a>}
             </div>
         </article>
     )
