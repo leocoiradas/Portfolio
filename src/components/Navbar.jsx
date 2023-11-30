f5faec0f708b5f6a1857af54d4add240436988a7
leocoiradas <coiradasalan@gmail.com>
2023-11-30 14:26:40 -0300

    Navbar: Ajustes de estilos para dispositivos móviles
    Se añadieron estilos que serán aplicados en dispositivos más pequeños, los estilos que el navbar tenía anteriormente fueron modificados para que se apliquen en dispositivos con pantallas más grandes.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,10 +25,12 @@ function Navbar() {
     },
     
   ];
-  return(
-    <ul className="flex justify-center items-center gap-2">
-      {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300">{link.name}</Link>))}
-    </ul>
+  
+  
+  return(  
+      <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center items-center gap-2">
+        {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300">{link.name}</Link>))}
+      </ul> 
   );
 }
 
