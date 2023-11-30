1ab51282ff1f8a133ca750057928fefacf1a5744
leocoiradas <coiradasalan@gmail.com>
2023-11-30 14:56:12 -0300

    ProjectCard: Ajuste de alineación según tamaño de pantalla
    Se ajustó la alineación de los elementos correspondientes al nombre, descripción y botón de detalles para que alineen al centro en pantallas de dispositivos móviles y a la izquierda en dispositivos con pantallas más grandes. Lo mismo se hizo con el párrafo que contiene la descripción del proyecto.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,9 +7,9 @@ function ProjectCard({project}) {
                 <div className="h-[10rem] md:h-[16rem] w-full">
                     <img className="object-fill w-full h-full" src={project.images[0]} alt={project.name} />
                 </div>
-                <div className="flex flex-col px-6 py-2 gap-2">
+                <div className="flex flex-col justify-center items-center lg:justify-start lg:items-start px-6 py-2 gap-2">
                     <div className="font-bold text-xl mb-2 text-white">{project.name}</div>
-                    <p className="text-white text-base">
+                    <p className="text-white text-base text-center lg:text-start">
                         {project.shortDesc}
                     </p>
                     <Link to={`/projects/${project._id}`} className="w-40 rounded-md text-center bg-purple-500 hover:bg-purple-700 text-white p-2">More Details</Link>
