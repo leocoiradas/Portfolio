908d7bae49009baa0ae8b9c04f46c33a0afe9031
leocoiradas <coiradasalan@gmail.com>
2023-12-01 14:15:17 -0300

    Modificacion de ícono al abrir el botón hamburguesa
    Se reemplazó la animación del botón abriéndose por un ícono estático de una X obtenida de React Icons, adicionalmente se añadieron algunas clases para darle estilos al botón.

--- src/components/HamburgerButton.jsx
+++ src/components/HamburgerButton.jsx
@@ -1,19 +1,28 @@
 import React from "react";
+import { VscChromeClose } from "react-icons/vsc";
 
+function HamburgerButton({ buttonSwitch, openSwitch, children }) {
+    return (
+        <div className="flex flex-col">
+            <div className="w-full flex justify-center items-center">
+                <button onClick={buttonSwitch} className="flex flex-col justify-center items-center  p-4 rounded-md border-2 border-transparent transition-all duration-300 lg:hidden">
+                    {openSwitch ?
+                        <VscChromeClose className="mb-1 text-white text-5xl rounded-md border-2 border-transparent font-bold ease-in-out duration-300" />
+                    :
+                        <>
+                            <div className={`line w-8 h-1 mb-1 bg-white`}> </div>
+                            <div className={`line w-8 h-1 mb-1 bg-white`}></div>
+                            <div className={`line w-8 h-1 mb-1 bg-white`}></div>
+                        </>
+                    }
+                </button>
+            </div>
 
-function HamburgerButton ({buttonSwitch, openSwitch, children}){
-    return(
-        <div>
-            <button onClick={buttonSwitch} className="flex flex-col w-full lg:hidden">
-                <div className={`line w-6 h-1 mb-1 bg-white ${openSwitch ? 'rotate-45' : ''}`}> </div>
-                <div className={`line w-6 h-1 mb-1 bg-white ${openSwitch ? 'opacity-0' : ''}`}></div>
-                <div className={`line w-6 h-1 mb-1 bg-white ${openSwitch ? '-rotate-45' : ''}`}></div>
-            </button>
             <div className="block relative z-10">
                 {openSwitch ? children : null}
             </div>
         </div>
-        
+
     )
 }
 
