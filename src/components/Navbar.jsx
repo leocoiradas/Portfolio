93d33e98dad75cc84f113f5ad5eac311aa1fe4a8
leocoiradas <coiradasalan@gmail.com>
2023-12-01 14:17:58 -0300

    Navbar: Aumento de tamaño del texto en pantallas pequeñas
    Se añadieron clases que permiten un aumento de tamaño del texto, esto en pantallas pequeñas, mientras que en pantallas grandes mantendrá un tamaño normal.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ function Navbar() {
   
   return(  
       <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center items-center gap-2">
-        {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300">{link.name}</Link>))}
+        {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">{link.name}</Link>))}
       </ul> 
   );
 }
