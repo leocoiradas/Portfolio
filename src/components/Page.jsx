12f678c33cfa5b53591c70ab2129b75dfc08c79f
leocoiradas <coiradasalan@gmail.com>
2023-12-05 23:53:43 -0300

    Page: Adaptado ancho y texto en dispositivos móviles
    Se adaptó el ancho del article que contiene el texto para que haga efecto en dispositivos con pantallas grandes, se hizo lo mismo con el tamaño del texto para que el texto grande solo haga efecto en pantallas grandes.

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Page({children, info}){
     return(
         <main className="flex flex-col justify-center items-center min-h-[90dvh] w-full gap-4 p-3">
-            <article className="w-[75vw] flex flex-col justify-center items-center p-5 text-xl text-center text-white gap-5">
+            <article className="lg:w-[75vw] flex flex-col justify-center items-center p-5 lg:text-xl text-center text-white gap-5">
                 <h1 className="text-3xl text-purple-400 font-sans">{info.title}</h1>
                 <p className="font-mono">{info.comment}</p>
                 <p className="text-2xl text-yellow-500">{info.icon}</p>
