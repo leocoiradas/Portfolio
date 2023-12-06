e2292bb67e5a86f5ba3b169bfda63bfdda638010
leocoiradas <coiradasalan@gmail.com>
2023-12-06 18:08:03 -0300

    Curriculum: Alineado texto del botón al centro del mismo

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -16,7 +16,7 @@ function Curriculum(){
             <div className="flex flex-col justify-center items-center lg:items-start text-center lg:text-start flex-1 gap-3">
                 <h2 className={"font-sans text-purple-500 text-2xl"}>Curriculum</h2>
                 <p className="font-mono text-xl text-white">{message}</p>
-                <a href="/src/assets/Curriculum/CV_Alan_Coiradas_Full_Stack_MERN_Developer.pdf" download className="w-36 inline-flex justify-start items-center text-center bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2 gap-2">{downloadIcon} Download</a>
+                <a href="/src/assets/Curriculum/CV_Alan_Coiradas_Full_Stack_MERN_Developer.pdf" download className="w-36 inline-flex justify-center items-center text-center bg-cyan-500 hover:bg-blue-700 rounded-md px-6 py-2 gap-2">{downloadIcon} Download</a>
             </div>
         </article>
     )
