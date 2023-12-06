1a9d147f792cb9343e1b407a31f85f6d916ae78c
leocoiradas <coiradasalan@gmail.com>
2023-12-06 18:13:55 -0300

    Header: Position sticky + color de fondo
    Se añadió una posición sticky al header para que cuando se haga scroll por la página este se mantenga fijo en su posición adquiriendo una posición relativa dentro de la página. se le subió el z-index al mismo para posicionar al header sobre el contenido de la página (especificamente para cuando adquiere su posición relativa). Adicionalmente al Header se le añadió un fondo negro para hacerlo resaltar al momento de hacer scroll por la página.

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,7 +3,7 @@ import NavbarFunction from "../components/NavbarFunction";
 
 function Header(){
     return(
-        <header className="flex justify-between items-center w-full min-h-[10dvh] p-3">
+        <header className="flex justify-between items-center w-full min-h-[10dvh] p-3 sticky top-0 z-50 bg-black">
             <div className="p-3">
                 <h1 className="text-white">Portfolio</h1>
             </div>
