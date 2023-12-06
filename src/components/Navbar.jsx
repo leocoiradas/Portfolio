88435fa48bd977ccc68605c290aa15946e2cdae2
leocoiradas <coiradasalan@gmail.com>
2023-12-05 23:44:20 -0300

    Navbar: Cambio en la alineación de los elementos del Navbar
    Se cambió la alineación de los elementos del centro al fin derecho del contenedor en dispositivos móviles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ function Navbar() {
   
   
   return(  
-      <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center items-center gap-2">
+      <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center lg:items-center items-end gap-2">
         {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">{link.name}</Link>))}
       </ul> 
   );
