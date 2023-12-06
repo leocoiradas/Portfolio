8af0941ac0e4fdab5eabead3601f5d8230fce857
leocoiradas <coiradasalan@gmail.com>
2023-12-05 23:34:29 -0300

    NavbarFunction: Resuelto error de la pantalla difuminada
    Se corrigió un error que ocurría cuando se dejaba abierto el navbar en dispositivos móviles y luego se agrandaba la pantalla el navbar permanecía abierto lo que provocaba que el fondo difuminado del navbar permanezca en la versión de escritorio.
    Utilizando la librería react-responsive y utilizando el metodo useMediaQuery se estableció un minimo de 1024px y utilizando el useEffect se establece que se establezca como falso la variable open cuando la variable isDesktop sea verdadera (o sea cuando el tamaño de la pantalla sea como mínimo de 1024px) esa verificacion se ejecuta cada vez que cambie el valor de la variable isDesktop.

--- src/components/NavbarFunction.jsx
+++ src/components/NavbarFunction.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import Navbar from "./Navbar";
 import HamburgerButton from "./HamburgerButton";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useMediaQuery } from "react-responsive";
 
 function NavbarFunction(){
     const [open, setOpen] = useState(false)
+    const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" })
     const handleNavbar = () => {
         setOpen(!open)
     }
+  useEffect(() => {
+    if (isDesktop) {
+      setOpen(false);
+    }
+  }, [isDesktop]);
     return(
         <div className={`${open ? " flex justify-end fixed  inset-0 backdrop-blur-md bg-black/30 bg-black bg-opacity-50 z-50" : null}`}>
             <div className="lg:hidden flex justify-end h-full">
