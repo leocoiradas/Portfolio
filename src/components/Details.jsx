739bb2912eeb58c78d1d3fa7c8cebc9dec65fbfc
leocoiradas <coiradasalan@gmail.com>
2023-12-05 23:37:40 -0300

    Details: Modificado el ancho en pantallas grandes
    Se modifico el ancho del contenedor principal y el tamaño del texto en dispositivos móviles.

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -9,7 +9,7 @@ function Details() {
   };
 
   return (
-    <article className="flex flex-col lg:flex-row justify-center items-center w-[85dvw] gap-3 p-4 text-2xl text-white border-2 rounded-md border-purple-400 backdrop-blur-md bg-black/60">
+    <article className="flex flex-col lg:flex-row justify-center items-center lg:w-[75dvw] gap-3 p-4 text-2xl text-white border-2 rounded-md border-purple-400 backdrop-blur-md bg-black/60">
         <article className="flex flex-col justify-center items-center flex-1">
             <article className="w-48 h-48">
                 <img src={profileImg} alt={details.name} classname="object-cover w-full h-full" />
@@ -18,7 +18,7 @@ function Details() {
             <h4 className="text-purple-700">{details.title}</h4>
       </article>
       <article className="flex flex-col gap-4 lg:w-2/3">
-        <p className="text-center text-sm lg:text-start lg:text-2xl">{details.message}</p>
+        <p className="text-center text-base lg:text-start lg:text-2xl">{details.message}</p>
       </article>
     </article>
   );
