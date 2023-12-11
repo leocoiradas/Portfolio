168f22ea0e7dddd6d8a2ef21bc33388b31af3ead
leocoiradas <coiradasalan@gmail.com>
2023-12-11 18:55:27 -0300

    Implementación de sintaxis de I18next en navBar

--- src/components/header_components/Navbar.jsx
+++ src/components/header_components/Navbar.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 function Navbar() {
+  const { t } = useTranslation("global")
   const links = [
     {
-      name: "Home",
+      name: "header.navbar.home",
       path: "/",
     },
     {
-        name: "About me",
+        name: "header.navbar.about",
         path: "/about"
     },
     {
-      name: "Projects",
+      name: "header.navbar.projects",
       path: "/projects"
     },
     {
-      name: "Certifications",
+      name: "header.navbar.certifications",
       path: "/certifications"
     },
     {
-       name: "Contact",
+       name: "header.navbar.contact",
        path: "/contact" 
     },
     
@@ -29,7 +31,11 @@ function Navbar() {
   
   return(  
       <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center lg:items-center items-end gap-2">
-        {links.map((link, i) => (<Link key={i} to={link.path} className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">{link.name}</Link>))}
+        {links.map((link, i) => (
+          <Link key={i} to={link.path} className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">
+            {t(link.name)}
+          </Link>
+            ))}
       </ul> 
   );
 }
