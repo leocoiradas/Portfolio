e51571ae153c0a80309991bb4fca48df28ec472e
leocoiradas <coiradasalan@gmail.com>
2023-12-12 21:06:49 -0300

    Implementación de LanguageSwitch en navbar

--- src/components/header_components/Navbar.jsx
+++ src/components/header_components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import LanguageSwitch from "./LanguageSwitch";
 
 function Navbar() {
   const { t } = useTranslation("global")
@@ -36,6 +37,7 @@ function Navbar() {
             {t(link.name)}
           </Link>
             ))}
+        <LanguageSwitch />
       </ul> 
   );
 }
