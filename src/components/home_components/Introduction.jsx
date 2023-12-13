0358d581bda268238124247d7bb2a65383f0db5c
leocoiradas <coiradasalan@gmail.com>
2023-12-13 15:43:12 -0300

    Introduction: Se modificó la fuente del texto del componente

--- src/components/home_components/Introduction.jsx
+++ src/components/home_components/Introduction.jsx
@@ -5,7 +5,7 @@ import { useTranslation } from "react-i18next";
 function Introduction() {
     const { t } = useTranslation("global")
     return (
-        <section className="flex justify-center items-center p-4 w-[75vw] gap-4 border-2 rounded-md border-purple-500 min-h-[50vh] backdrop-blur-md bg-black/60">
+        <section className="flex justify-center items-center p-4 w-[75vw] gap-4 border-2 rounded-md border-purple-500 min-h-[50vh] backdrop-blur-md bg-black/60 font-mono">
             <article className="flex flex-col justify-center items-center w-full gap-4">
                 <p className="lg:text-2xl text-center text-white">{t("home.message")}</p>
             </article>
