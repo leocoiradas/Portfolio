8498d0037ed5d0c3608e6348432f0f775dfbc7f5
leocoiradas <coiradasalan@gmail.com>
2023-12-13 16:56:50 -0300

    Certifications: Implementado sistema de traducciones

--- src/pages/Certifications.jsx
+++ src/pages/Certifications.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import CertificationList from "../components/certifications_components/CertificationsList";
 import Page from "../components/Page";
 import { BiSolidCertification } from "react-icons/bi";
+import { useTranslation } from "react-i18next";
 
 function Certifications(){
+    const { t } = useTranslation("global")
     const certificationsText = {
-        title: "Studies and Certifications",
-        comment: "These are the certifications I Have so far. I'm always looking foward to expand my knowledge!",
+        title: t("page.certifications.title"),
+        comment: t("page.certifications.desc"),
         icon: <BiSolidCertification />
     }
     return(
