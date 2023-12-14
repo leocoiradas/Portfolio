84adf591d67c35078bcb865e9af27dbc65417151
leocoiradas <coiradasalan@gmail.com>
2023-12-14 13:11:28 -0300

    ProjectDetails: Cambio de color del texto del botón de regreso

--- src/pages/ProjectDetails.jsx
+++ src/pages/ProjectDetails.jsx
@@ -29,7 +29,7 @@ function ProjectDetails() {
     return (
         <Page info={detailsText}>
            {projectDetails && projectDetails.images ? <DetailsCard details={ projectDetails } /> : null}
-            <Link to="/projects" className="bg-purple-500 hover:bg-purple-800 text-white rounded-md p-3">{t("projectDetails.backButton")}</Link>
+            <Link to="/projects" className="bg-purple-500 hover:bg-purple-800 rounded-md p-3">{t("projectDetails.backButton")}</Link>
         </Page>
     )
 }
