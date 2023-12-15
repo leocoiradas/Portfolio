6862bddcd03f4621ec8a2ae7035fafa1d169944f
leocoiradas <coiradasalan@gmail.com>
2023-12-15 16:32:47 -0300

    Implementacion de apiUrl

--- src/store/actions/proyectDetailsAction.js
+++ src/store/actions/proyectDetailsAction.js
@@ -1,9 +1,10 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+import apiURL from "../../api";
 
 export const get_proyect_details = createAsyncThunk('get_proyect_details', async(obj) => {
     try {
-        const response = await axios.get(`http://localhost:8000/api/proyects/${obj.id}`)
+        const response = await axios.get(`${apiURL}/api/proyects/${obj.id}`)
         console.log(response)
         return {
             proyect_details: response.data.proyectById
