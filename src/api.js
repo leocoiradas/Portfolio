5cdeb1874305fffc10548f12bfcbe4c379146f32
leocoiradas <coiradasalan@gmail.com>
2023-12-19 18:22:51 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,4 @@
-
-
-let apiURL = import.meta.env.VITE_BACK_URL
-//let apiURL = "http://localhost:8000"
+let apiURL = "http://localhost:8000"
 
 if(process.env.NODE_ENV === "production"){
     apiURL = import.meta.env.VITE_BACK_URL
