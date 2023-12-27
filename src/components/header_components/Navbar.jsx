d55280bc9c0f48a1385d59bb00ffff92b386affd
leocoiradas <coiradasalan@gmail.com>
2023-12-27 15:23:22 -0300

    Añadido prop navFunction al navbar
    Se añadió una prop a los botones del navbar llamada navFunction, la cual será usada para cambiar el estado de apertura/cierre del botón hamburguesa.

--- src/components/header_components/Navbar.jsx
+++ src/components/header_components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import LanguageSwitch from "./LanguageSwitch";
 
-function Navbar() {
+function Navbar({navFunction}) {
   const { t } = useTranslation("global")
   const links = [
     {
@@ -33,7 +33,7 @@ function Navbar() {
   return(  
       <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center lg:items-center items-end gap-2">
         {links.map((link, i) => (
-          <Link key={i} to={link.path} className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">
+          <Link key={i} to={link.path} onClick={navFunction} className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">
             {t(link.name)}
           </Link>
             ))}
