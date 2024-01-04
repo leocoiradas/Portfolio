9f69f852c15169577f4f69db8c24da4517273fd4
leocoiradas <coiradasalan@gmail.com>
2024-01-04 16:20:25 -0300

    Añadido value al select del selector de idiomas

--- src/components/header_components/LanguageSwitch.jsx
+++ src/components/header_components/LanguageSwitch.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import { useState, useEffect } from "react";
 
-
-
-function LanguageSwitch(){
-    const { t, i18n } = useTranslation("global")
-    let currentLanguage = i18n.language
-    const [language, setLanguage] = useState(currentLanguage)
+function LanguageSwitch() {
+    const { t, i18n } = useTranslation("global");
+    let currentLanguage = i18n.language;
+    const [language, setLanguage] = useState(currentLanguage);
 
     useEffect(() => {
         i18n.changeLanguage(language)
@@ -17,12 +15,17 @@ function LanguageSwitch(){
         setLanguage(event.target.value)
     }
 
-    return(
-        <select onChange={switchLanguage} className="w-28 text-2xl lg:text-base bg-transparent text-white text-center p-1 rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 appearance-none">
-            <option value="es" className="bg-black text-base">{t("header.language.es")}</option>
-            <option value="en" className="bg-black text-base">{t("header.language.en")}</option>
+
+    return (
+        <select onChange={switchLanguage} value={language} className="w-28 text-2xl lg:text-base bg-transparent text-white text-center p-1 rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 appearance-none">
+            <option value="es" className="bg-black text-base">
+                {t("header.language.es")}
+            </option>
+            <option value="en" className="bg-black text-base">
+                {t("header.language.en")}
+            </option>
         </select>
-    )
+    );
 }
 
-export default LanguageSwitch
+export default LanguageSwitch;
