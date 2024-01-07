5c4af133382989fbb0f1433c1b900ce75b0bea9f
leocoiradas <coiradasalan@gmail.com>
2024-01-06 22:28:54 -0300

    Corrección de rutas y nombre del objeto que aloja los datos

--- src/store/actions/projectDetailsAction.js
+++ src/store/actions/projectDetailsAction.js
@@ -4,10 +4,10 @@ import apiURL from "../../api";
 
 export const get_project_details = createAsyncThunk('get_project_details', async(obj) => {
     try {
-        const response = await axios.get(`${apiURL}/proyects/${obj.id}`)
+        const response = await axios.get(`${apiURL}/projects/${obj.id}`)
         console.log(response)
         return {
-            project_details: response.data.proyectById
+            project_details: response.data.projectById
         }
     } catch (error) {
         console.log(error)
