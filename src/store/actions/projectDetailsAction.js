15e817f5b05a11265c07076f62370210faa5e057
leocoiradas <coiradasalan@gmail.com>
2024-01-08 18:38:29 -0300

    Creación de action clear_details
    Se creó la acción que se encarga de limpiar el estado de projectDetails.

--- src/store/actions/projectDetailsAction.js
+++ src/store/actions/projectDetailsAction.js
@@ -5,11 +5,21 @@ import apiURL from "../../api";
 export const get_project_details = createAsyncThunk('get_project_details', async(obj) => {
     try {
         const response = await axios.get(`${apiURL}/projects/${obj.id}`)
-        console.log(response)
         return {
             project_details: response.data.projectById
         }
     } catch (error) {
+        console.log(error)
+    }
+})
+
+export const clear_details = createAsyncThunk('clear_details', async() => {
+    try {
+        return{
+            project_details: {}
+        }
+    }
+    catch (error){
         console.log(error)
     }
 })
