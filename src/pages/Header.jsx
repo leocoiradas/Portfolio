eacb2a1bb3b70fb9f390c6fdf88e6d773706eb5c
leocoiradas <coiradasalan@gmail.com>
2024-01-08 18:32:49 -0300

    Agregada sombra en dispositivos móviles

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,7 +3,7 @@ import NavbarFunction from "../components/header_components/NavbarFunction";
 
 function Header(){
     return(
-        <header className="flex justify-between items-center w-full min-h-[10dvh] p-3 sticky top-0 z-50 bg-black">
+        <header className="flex justify-between items-center w-full min-h-[10dvh] p-3 sticky top-0 z-50 bg-black shadow-md shadow-white md:shadow-none">
             <div className="p-3">
                 <h1 className="text-xl lg:text-lg text-white">Alan Coiradas</h1>
             </div>
