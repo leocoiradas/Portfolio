378c1ca822db4177320b27c3c3ab279f5273d1b9
leocoiradas <coiradasalan@gmail.com>
2024-01-09 15:03:32 -0300

    Cambio de name por aria-label en el botón del navbar

--- src/components/header_components/HamburgerButton.jsx
+++ src/components/header_components/HamburgerButton.jsx
@@ -7,7 +7,7 @@ function HamburgerButton({ buttonSwitch, openSwitch, children }) {
     return (
         <div className="flex flex-col">
             <div className="w-full flex justify-end items-center">
-                <button name={openSwitch ? t("navbar-button.name-close") : t("navbar-button.name-open")} onClick={buttonSwitch} className="flex flex-col justify-center items-center  p-4 rounded-md border-2 border-transparent transition-all duration-300 lg:hidden">
+                <button aria-label={openSwitch ? t("navbar-button.name-close") : t("navbar-button.name-open")} onClick={buttonSwitch} className="flex flex-col justify-center items-center  p-4 rounded-md border-2 border-transparent transition-all duration-300 lg:hidden">
                     {openSwitch ?
                         <VscChromeClose className="mb-1 text-white text-5xl rounded-md border-2 border-transparent font-bold ease-in-out duration-300" />
                     :
