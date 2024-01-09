71653808275ef683fdcf0cc252f5329ff8e15331
leocoiradas <coiradasalan@gmail.com>
2024-01-09 15:27:36 -0300

    Se agregó la propiedad alt a la imagen del certificado

--- src/components/certifications_components/CertificationCard.jsx
+++ src/components/certifications_components/CertificationCard.jsx
@@ -6,7 +6,7 @@ function CertificationCard({ certification }) {
     return (
         <article className="flex flex-col justify-center items-center lg:w-[24rem] border-2 rounded-md bg-purple-600 hover:scale-[110%] ease-linear duration-300 p-3 gap-3">
             <div className="lg:max-h-[14rem]">
-                <img src={certification.image} className="w-full h-full rounded-md"/>
+                <img src={certification.image} alt={certification.title} className="w-full h-full rounded-md"/>
             </div>
             <div className="flex flex-col justify-center items-center gap-3 p-3">
                 <h3 className="text-xl lg:text-2xl text-white">{certification.title}</h3>
