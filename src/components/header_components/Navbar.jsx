c4bce682d19f60e21c1135175da73aeedfd5921f
leocoiradas <coiradasalan@gmail.com>
2024-01-09 19:37:04 -0300

    Navbar: Se extendió el gap en pantallas pequeñas

--- src/components/header_components/Navbar.jsx
+++ src/components/header_components/Navbar.jsx
@@ -32,13 +32,13 @@ function Navbar({navFunction}) {
   
   
   return(  
-      <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center lg:items-center items-end gap-2">
+      <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center lg:items-center items-end gap-5 md:gap-2">
         {links.map((link, i) => (
           <li><Link key={i} to={link.path} onClick={ navFunction }  className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">
-          {t(link.name)}
-        </Link></li>
+            {t(link.name)}
+          </Link></li>
             ))}
-        <li><LanguageSwitch /></li>
+          <li><LanguageSwitch /></li>
       </ul> 
   );
 }
