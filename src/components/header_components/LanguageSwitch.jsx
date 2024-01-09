d901058e3c4d1d31c5d5a247de20a0a475e57181
leocoiradas <coiradasalan@gmail.com>
2024-01-09 16:19:56 -0300

    Añadido atributo label con el nombre del idioma a cada option del selector de idiomas

--- src/components/header_components/LanguageSwitch.jsx
+++ src/components/header_components/LanguageSwitch.jsx
@@ -18,12 +18,8 @@ function LanguageSwitch() {
 
     return (
         <select onChange={switchLanguage} value={language} className="w-28 text-2xl lg:text-base bg-transparent text-white text-center p-1 rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 appearance-none">
-            <option value="es" className="bg-black text-base">
-                {t("header.language.es")}
-            </option>
-            <option value="en" className="bg-black text-base">
-                {t("header.language.en")}
-            </option>
+            <option label={t("header.language.es")} value="es" className="bg-black text-base" />
+            <option label={t("header.language.en")} value="en" className="bg-black text-base" />
         </select>
     );
 }
