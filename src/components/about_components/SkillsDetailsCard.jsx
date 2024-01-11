00bc54a05276184c7a269099611d2b18ef9b2061
leocoiradas <coiradasalan@gmail.com>
2024-01-11 19:14:39 -0300

    SkillsDetailsCard: Agregado div para alinear mejor los elementos de la lista de habilidades

--- src/components/about_components/SkillsDetailsCard.jsx
+++ src/components/about_components/SkillsDetailsCard.jsx
@@ -6,14 +6,17 @@ function SkillsDetailsCard({ arr, collectionName }){
     return(
         <details className="w-full md:w-[50dvw] flex flex-col justify-center items-center rounded-md border-2 border-purple-500 gap-5 py-3 px-2">
             <summary className="md:text-2xl text-amber-500 text-center font-mono">{t(collectionName)}</summary>
-            <ul className="md:h-56 flex flex-col justify-center items-center flex-wrap p-4 gap-3">
-                {arr.map((element, i) => (
-                    <li key={i} className="flex justify-start items-center gap-2 text-2xl md:text-2xl font-mono">
-                        <p className="text-yellow-500"><b>{element.icon}</b></p>
-                        <p className="text-white">{element.skill}</p>
-                    </li>))
-                }
-            </ul>
+            <div className="divide-y bg-white"></div>
+            <div className="w-full flex justify-center items-center">
+                <ul className="md:h-56 flex flex-col justify-center items-center md:items-start flex-wrap p-4 gap-4">
+                    {arr.map((element, i) => (
+                        <li key={i} className="flex justify-start items-center gap-2 text-2xl md:text-2xl font-mono mx-3">
+                            <p className="text-yellow-500"><b>{element.icon}</b></p>
+                            <p className="text-white">{element.skill}</p>
+                        </li>))
+                    }
+                </ul>
+            </div>
         </details>
     )
 }
