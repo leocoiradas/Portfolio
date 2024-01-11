b8cdf274a98083911eac47965e658b8d07935524
leocoiradas <coiradasalan@gmail.com>
2024-01-11 19:44:56 -0300

    CertificationCard: Modificado el color de fondo de la tarjeta

--- src/components/certifications_components/CertificationCard.jsx
+++ src/components/certifications_components/CertificationCard.jsx
@@ -4,7 +4,7 @@ import { useTranslation } from "react-i18next";
 function CertificationCard({ certification }) {
     const { t } = useTranslation("global")
     return (
-        <article className="flex flex-col justify-center items-center lg:w-[24rem] border-2 rounded-md bg-purple-600 hover:scale-[110%] ease-linear duration-300 p-3 gap-3">
+        <article className="flex flex-col justify-center items-center lg:w-[24rem] border-2 rounded-md bg-purple-400 hover:scale-[110%] ease-linear duration-300 p-3 gap-3">
             <div className="lg:max-h-[14rem]">
                 <img src={certification.image} alt={certification.title} className="w-full h-full rounded-md"/>
             </div>
