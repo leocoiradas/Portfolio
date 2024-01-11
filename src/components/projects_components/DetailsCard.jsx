52bc932a93503536fa128cc2c63e9fda232a304a
leocoiradas <coiradasalan@gmail.com>
2024-01-11 19:18:35 -0300

    DetailsCard: Modificación del el tamaño del contenedor de la imagen

--- src/components/projects_components/DetailsCard.jsx
+++ src/components/projects_components/DetailsCard.jsx
@@ -16,7 +16,7 @@ function DetailsCard({ details }) {
     return (
         <section className="lg:max-w-[65dvw]">
             <div className="block rounded-md shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] bg-slate-900">
-                <div className="flex  snap-x snap-mandatory overflow-x-scroll scroll-smooth gap-3 md:gap-6">
+                <div className="flex h-60 w-full md:h-auto snap-x snap-mandatory overflow-x-scroll scroll-smooth gap-3 md:gap-6">
                     {details.images.map((image, i) => (
                             <img
                                 key={i}
