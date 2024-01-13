46547093560180111dd4d99c2fe87664ab2b280b
leocoiradas <coiradasalan@gmail.com>
2024-01-13 19:31:49 -0300

    Navbar: Añadido identificador de sección
    Haciendo uso del hook useLocation se obtiene la ruta actual del sitio web. Por medio de un ternario en la clase del elemento Link comparamos si la ruta actual es igual a link.path (que representa a la ruta donde redirige el botón). Dependiendo de si es verdadero o no, se le asignan clases para colorear el botón, destacando aquel que tenga su atributo path igual a la ruta actual haciendo como un marcador para indicar en qué sección de la página se encuentra el usuario.

--- src/components/header_components/Navbar.jsx
+++ src/components/header_components/Navbar.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import LanguageSwitch from "./LanguageSwitch";
+import { useLocation } from "react-router-dom";
 
 function Navbar({navFunction}) {
   const { t } = useTranslation("global")
+  const location = useLocation()
+  const pathName = location.pathname
   
   const links = [
     {
@@ -18,6 +21,7 @@ function Navbar({navFunction}) {
     {
       name: "header.navbar.projects",
       path: "/projects"
+
     },
     {
       name: "header.navbar.certifications",
@@ -34,7 +38,7 @@ function Navbar({navFunction}) {
   return(  
       <ul className="flex flex-col w-[60dvw] lg:flex-row lg:static lg:w-auto lg:bg-none justify-center lg:items-center items-end gap-5 md:gap-2">
         {links.map((link, i) => (
-          <li key={i}><Link to={link.path} onClick={ navFunction }  className="px-4 py-2 text-white text-center  text-2xl rounded-md border-2 border-transparent hover:border-purple-600 hover:text-purple-600 ease-in duration-300 lg:text-base">
+          <li key={i}><Link to={link.path} onClick={ navFunction }  className={`px-4 py-2 text-center text-2xl rounded-md border-2 border-transparent ease-in duration-300 lg:text-base ${link.path == pathName ? "bg-amber-400 text-black font-medium" : "text-white hover:border-purple-600 hover:text-purple-600"}`}>
             {t(link.name)}
           </Link></li>
             ))}
